6831d980e17ec0465501dce0741bac40fb628abc
Rahul Javalagi <49891995+rxj171630@users.noreply.github.com>
2023-01-29 21:42:11 -0600

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+{
+	while true
+	do
+		# Start bot
+        git pull
+		node .
+		    echo "Recovering from crash!"
+        echo "If you wish to completely stop the bot process now, press CTRL-C before the time is up!"
+        echo "Starting up in:"
+        for i in 5 4 3 2 1
+        do
+            echo "$i..."
+            sleep 1
+        done
+        echo "Bot is starting up!"
+    done
+}
