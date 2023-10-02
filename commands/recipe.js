6edd923c49e28e1026b34d335ccb22fc89d958e5
Rahul Javalagi <17javalr@gmail.com>
2023-10-02 01:22:50 -0500

    Added poll reactions

--- commands/recipe.js
+++ commands/recipe.js
@@ -191,171 +191,192 @@ module.exports = {
       await interaction.channel.send("## Instructions:");
       if (JSON.stringify(myJSON.strInstructions).length > 1900) {
         await interaction.channel.send(`\`\`\`${split1}\`\`\``);
-        await interaction.channel.send(`\`\`\`${split2}\`\`\``);
+        message = await interaction.channel.send(`\`\`\`${split2}\`\`\``);
       } else {
-        await interaction.channel.send(`\`\`\`${instructions}\`\`\``);
+        message = await interaction.channel.send(`\`\`\`${instructions}\`\`\``);
       }
-    } else if (interaction.options.getSubcommand() === "category")
-      input = interaction.options.getString("category");
-    query = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${input}`;
-    resp = await fetch(query);
-    myJSON = await resp.json();
-    rand = Math.floor(Math.random() * myJSON.meals.length);
-    myJSON = myJSON.meals[rand];
-    query = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${myJSON.idMeal}`;
-    resp = await fetch(query);
-    myJSON = await resp.json();
-    myJSON = myJSON.meals[0];
 
-    //console.log(myJSON);
-
-    const embed = new EmbedBuilder()
-      .setColor(`#32a852`)
-      .setTitle(myJSON.strMeal)
-      .setThumbnail(myJSON.strMealThumb)
-      .addFields(
-        {
-          name: "Category:",
-          value: `${myJSON.strCategory ?? "none"}`,
-          inline: false,
-        },
-        {
-          name: "Country:",
-          value: `${myJSON.strArea ?? "N/A"}`,
-          inline: false,
-        }
+      setTimeout(
+        () => message.react(`<:DrakeNo:1158286870661320725>`),
+        ms("0.5 sec")
       );
-    mes = "";
-    ing = "";
-    if (myJSON.strSource) {
-      embed.setURL(myJSON.strSource);
-    }
-    if (myJSON.strTags != "") {
-      embed.addFields({
-        name: "Tags:",
-        value: `${myJSON.strTags ?? "none"}`,
-        inline: false,
-      });
-    }
+      setTimeout(
+        () => message.react(`<:DrakeYes:1158286868954222633>`),
+        ms("0.5 sec")
+      );
+    } else if (interaction.options.getSubcommand() === "category") {
+      input = interaction.options.getString("category");
+      query = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${input}`;
+      resp = await fetch(query);
+      myJSON = await resp.json();
+      rand = Math.floor(Math.random() * myJSON.meals.length);
+      myJSON = myJSON.meals[rand];
+      query = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${myJSON.idMeal}`;
+      resp = await fetch(query);
+      myJSON = await resp.json();
+      myJSON = myJSON.meals[0];
 
-    if (myJSON.strIngredient1 != null) {
-      mes += myJSON.strMeasure1 + "\n";
-      ing += myJSON.strIngredient1 + "\n";
-    }
-    if (myJSON.strIngredient2 != null) {
-      mes += myJSON.strMeasure2 + "\n";
-      ing += myJSON.strIngredient2 + "\n";
-    }
-    if (myJSON.strIngredient3 != null) {
-      mes += myJSON.strMeasure3 + "\n";
-      ing += myJSON.strIngredient3 + "\n";
-    }
-    if (myJSON.strIngredient4 != null) {
-      mes += myJSON.strMeasure4 + "\n";
-      ing += myJSON.strIngredient4 + "\n";
-    }
-    if (myJSON.strIngredient5 != null) {
-      mes += myJSON.strMeasure5 + "\n";
-      ing += myJSON.strIngredient5 + "\n";
-    }
-    if (myJSON.strIngredient6 != null) {
-      mes += myJSON.strMeasure6 + "\n";
-      ing += myJSON.strIngredient6 + "\n";
-    }
-    if (myJSON.strIngredient7 != null) {
-      mes += myJSON.strMeasure7 + "\n";
-      ing += myJSON.strIngredient7 + "\n";
-    }
-    if (myJSON.strIngredient8 != null) {
-      mes += myJSON.strMeasure8 + "\n";
-      ing += myJSON.strIngredient8 + "\n";
-    }
-    if (myJSON.strIngredient9 != null) {
-      mes += myJSON.strMeasure9 + "\n";
-      ing += myJSON.strIngredient9 + "\n";
-    }
-    if (myJSON.strIngredient10 != null) {
-      mes += myJSON.strMeasure10 + "\n";
-      ing += myJSON.strIngredient10 + "\n";
-    }
-    if (myJSON.strIngredient11 != null) {
-      mes += myJSON.strMeasure11 + "\n";
-      ing += myJSON.strIngredient11 + "\n";
-    }
-    if (myJSON.strIngredient12 != null) {
-      mes += myJSON.strMeasure12 + "\n";
-      ing += myJSON.strIngredient12 + "\n";
-    }
-    if (myJSON.strIngredient13 != null) {
-      mes += myJSON.strMeasure13 + "\n";
-      ing += myJSON.strIngredient13 + "\n";
-    }
-    if (myJSON.strIngredient14 != null) {
-      mes += myJSON.strMeasure14 + "\n";
-      ing += myJSON.strIngredient14 + "\n";
-    }
-    if (myJSON.strIngredient15 != null) {
-      mes += myJSON.strMeasure15 + "\n";
-      ing += myJSON.strIngredient15 + "\n";
-    }
-    if (myJSON.strIngredient16 != null) {
-      mes += myJSON.strMeasure16 + "\n";
-      ing += myJSON.strIngredient16 + "\n";
-    }
-    if (myJSON.strIngredient17 != null) {
-      mes += myJSON.strMeasure17 + "\n";
-      ing += myJSON.strIngredient17 + "\n";
-    }
-    if (myJSON.strIngredient18 != null) {
-      mes += myJSON.strMeasure18 + "\n";
-      ing += myJSON.strIngredient18 + "\n";
-    }
-    if (myJSON.strIngredient19 != null) {
-      mes += myJSON.strMeasure19 + "\n";
-      ing += myJSON.strIngredient19 + "\n";
-    }
-    if (myJSON.strIngredient20 != null) {
-      mes += myJSON.strMeasure20 + "\n";
-      ing += myJSON.strIngredient20 + "\n";
-    }
+      //console.log(myJSON);
 
-    embed
-      .addFields(
-        {
-          name: "Measurements:",
-          value: mes,
-          inline: true,
-        },
-        {
-          name: "Ingredients:",
-          value: ing,
-          inline: true,
-        }
-      )
-      .setTimestamp();
+      const embed = new EmbedBuilder()
+        .setColor(`#32a852`)
+        .setTitle(myJSON.strMeal)
+        .setThumbnail(myJSON.strMealThumb)
+        .addFields(
+          {
+            name: "Category:",
+            value: `${myJSON.strCategory ?? "none"}`,
+            inline: false,
+          },
+          {
+            name: "Country:",
+            value: `${myJSON.strArea ?? "N/A"}`,
+            inline: false,
+          }
+        );
+      mes = "";
+      ing = "";
+      if (myJSON.strSource) {
+        embed.setURL(myJSON.strSource);
+      }
+      if (myJSON.strTags != "") {
+        embed.addFields({
+          name: "Tags:",
+          value: `${myJSON.strTags ?? "none"}`,
+          inline: false,
+        });
+      }
 
-    if (myJSON.strYoutube != "") {
-      embed.addFields({
-        name: "Video Link:",
-        value: myJSON.strYoutube,
-        inline: false,
-      });
-    }
-    instructions = myJSON.strInstructions;
-    split1 = "";
-    split2 = "";
-    if (JSON.stringify(myJSON.strInstructions).length > 1900) {
-      index = instructions.substring(0, 1900).lastIndexOf("\n");
-      split1 = instructions.substring(0, index);
-      split2 = instructions.substring(index);
-    }
-    await interaction.reply({ embeds: [embed] });
-    await interaction.channel.send("## Instructions:");
-    if (JSON.stringify(myJSON.strInstructions).length > 1900) {
-      await interaction.channel.send(`\`\`\`${split1}\`\`\``);
-      await interaction.channel.send(`\`\`\`${split2}\`\`\``);
-    } else {
-      await interaction.channel.send(`\`\`\`${instructions}\`\`\``);
+      if (myJSON.strIngredient1 != null) {
+        mes += myJSON.strMeasure1 + "\n";
+        ing += myJSON.strIngredient1 + "\n";
+      }
+      if (myJSON.strIngredient2 != null) {
+        mes += myJSON.strMeasure2 + "\n";
+        ing += myJSON.strIngredient2 + "\n";
+      }
+      if (myJSON.strIngredient3 != null) {
+        mes += myJSON.strMeasure3 + "\n";
+        ing += myJSON.strIngredient3 + "\n";
+      }
+      if (myJSON.strIngredient4 != null) {
+        mes += myJSON.strMeasure4 + "\n";
+        ing += myJSON.strIngredient4 + "\n";
+      }
+      if (myJSON.strIngredient5 != null) {
+        mes += myJSON.strMeasure5 + "\n";
+        ing += myJSON.strIngredient5 + "\n";
+      }
+      if (myJSON.strIngredient6 != null) {
+        mes += myJSON.strMeasure6 + "\n";
+        ing += myJSON.strIngredient6 + "\n";
+      }
+      if (myJSON.strIngredient7 != null) {
+        mes += myJSON.strMeasure7 + "\n";
+        ing += myJSON.strIngredient7 + "\n";
+      }
+      if (myJSON.strIngredient8 != null) {
+        mes += myJSON.strMeasure8 + "\n";
+        ing += myJSON.strIngredient8 + "\n";
+      }
+      if (myJSON.strIngredient9 != null) {
+        mes += myJSON.strMeasure9 + "\n";
+        ing += myJSON.strIngredient9 + "\n";
+      }
+      if (myJSON.strIngredient10 != null) {
+        mes += myJSON.strMeasure10 + "\n";
+        ing += myJSON.strIngredient10 + "\n";
+      }
+      if (myJSON.strIngredient11 != null) {
+        mes += myJSON.strMeasure11 + "\n";
+        ing += myJSON.strIngredient11 + "\n";
+      }
+      if (myJSON.strIngredient12 != null) {
+        mes += myJSON.strMeasure12 + "\n";
+        ing += myJSON.strIngredient12 + "\n";
+      }
+      if (myJSON.strIngredient13 != null) {
+        mes += myJSON.strMeasure13 + "\n";
+        ing += myJSON.strIngredient13 + "\n";
+      }
+      if (myJSON.strIngredient14 != null) {
+        mes += myJSON.strMeasure14 + "\n";
+        ing += myJSON.strIngredient14 + "\n";
+      }
+      if (myJSON.strIngredient15 != null) {
+        mes += myJSON.strMeasure15 + "\n";
+        ing += myJSON.strIngredient15 + "\n";
+      }
+      if (myJSON.strIngredient16 != null) {
+        mes += myJSON.strMeasure16 + "\n";
+        ing += myJSON.strIngredient16 + "\n";
+      }
+      if (myJSON.strIngredient17 != null) {
+        mes += myJSON.strMeasure17 + "\n";
+        ing += myJSON.strIngredient17 + "\n";
+      }
+      if (myJSON.strIngredient18 != null) {
+        mes += myJSON.strMeasure18 + "\n";
+        ing += myJSON.strIngredient18 + "\n";
+      }
+      if (myJSON.strIngredient19 != null) {
+        mes += myJSON.strMeasure19 + "\n";
+        ing += myJSON.strIngredient19 + "\n";
+      }
+      if (myJSON.strIngredient20 != null) {
+        mes += myJSON.strMeasure20 + "\n";
+        ing += myJSON.strIngredient20 + "\n";
+      }
+
+      embed
+        .addFields(
+          {
+            name: "Measurements:",
+            value: mes,
+            inline: true,
+          },
+          {
+            name: "Ingredients:",
+            value: ing,
+            inline: true,
+          }
+        )
+        .setTimestamp();
+
+      if (myJSON.strYoutube != "") {
+        embed.addFields({
+          name: "Video Link:",
+          value: myJSON.strYoutube,
+          inline: false,
+        });
+      }
+      instructions = myJSON.strInstructions;
+      split1 = "";
+      split2 = "";
+      if (JSON.stringify(myJSON.strInstructions).length > 1900) {
+        index = instructions.substring(0, 1900).lastIndexOf("\n");
+        split1 = instructions.substring(0, index);
+        split2 = instructions.substring(index);
+      }
+      await interaction.reply({ embeds: [embed] });
+      await interaction.channel.send("## Instructions:");
+      if (JSON.stringify(myJSON.strInstructions).length > 1900) {
+        await interaction.channel.send(`\`\`\`${split1}\`\`\``);
+        message = await interaction.channel.send(`\`\`\`${split2}\`\`\``);
+      } else {
+        message = await interaction.channel.send(`\`\`\`${instructions}\`\`\``);
+      }
+
+      setTimeout(
+        () => message.react(`<:DrakeNo:1158286870661320725>`),
+        ms("0.5 sec")
+      );
+      setTimeout(
+        () => message.react(`<:DrakeYes:1158286868954222633>`),
+        ms("0.5 sec")
+      );
+      // message.react(`<:drake_no:946723622632976404>`);
+      // message.react(`<:drake_yes:946723680216555520>`);
     }
   },
 };
